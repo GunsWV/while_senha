854e68bae9e19b9225abc9b5917a01381fc462bc
GunsWV <guguwissenbach@hotmail.com>
2025-05-26 15:43:08 -0300

    Add files via upload

---
+++ while_senha.py
@@ -0,0 +1,9 @@
+senha = input("Digite a senha: ")
+
+while senha != "123456":
+    senha = input("Digite a senha: ")
+    if senha == "123456":
+        print("Senha Correta!")
+    else:
+        print("Senha incorreta! Tente Novamente")    
+ 
